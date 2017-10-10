c24fef5489235c75d57ec13b2745b261d052c328
tropikbrice <blabejof@gmail.com>
2017-10-10 18:49:48 +0200

    ajout exo 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+
+def translate(param)
+
+	vowels = ["a", "e","i" ,"o", "u", "y"]
+	alphabet =("a".."z").to_a
+	consonants = alphabet - vowels
+
+	long= param.length
+
+	if vowels.include?(param[0])
+		param + "ay"
+
+	#"cherry" => "errychay"
+	elsif consonants.include?(param[0]) && consonants.include?(param[1])
+		param[2..long] + param[0..1] + 'ay'
+
+	#banana =>ananabay - a mettre en dernier car cette condition annule celle avec 2 consonnes
+	elsif consonants.include?(param[0])
+    	param[1..-1] + param[0] + 'ay'
+
+	else
+		param
+	end
+
+end
