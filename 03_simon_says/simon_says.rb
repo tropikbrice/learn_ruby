be9bc72f2db9932886efb010ed89e086a6be0732
tropikbrice <blabejof@gmail.com>
2017-10-10 17:50:30 +0200

    exo 03 echo, repeat, capitalize titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def echo(param)
+	return "hello" if param =="hello"
+	return "bye" if param =="bye"
+end
+
+def shout(param)
+	return param.upcase
+end
+
+def repeat(param, nb=2)
+	( (param + " ") * nb ).strip
+end
+
+def start_of_word(param, nb)
+	param[0, nb]
+end
+
+def first_word(param)
+	z=param.split
+	z[0]
+end
+
+def titleize(param)
+
+	little_words = ["the","over","and"]
+
+	param.capitalize.split.map { |mot|
+		if little_words.include?(mot) 
+			mot
+		else
+			mot.capitalize
+		end
+	}.join(" ")
+end
+
